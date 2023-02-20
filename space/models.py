f252a0c024089003a106cbfad16900151afe1e96
locallab-seoul <locallab.seoul@gmail.com>
2023-02-21 00:18:06 +0900

    init model for space

--- space/models.py
+++ space/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Spaces(models.Model):
+
+    title       = models.CharField(max_length=200, verbose_name="공간명")
+    summary     = models.TextField(verbose_name="소개", null=True)
+    address     = models.TextField(verbose_name="주소", null=True)
+    created_at  = models.DateTimeField(auto_now_add=True, verbose_name="작성일")
+    updated_at  = models.DateTimeField(auto_now=True, verbose_name="최종수정일")
+
+    def __str__(self):
+        return self.title
--- space/views.py
+++ space/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Spaces
 
 # Create your views here.
+def index(request):
+    all_spaces = Spaces.objects.all().order_by('-id')
+    return render(request, 'index.html', {
+        'spaces': all_spaces,
+    })
