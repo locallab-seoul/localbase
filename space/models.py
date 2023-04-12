dc58c1699734006a3a7acadf76e8d06bb2f27268
locallab-seoul <locallab.seoul@gmail.com>
2023-04-13 01:51:57 +0900

    add insta / map link

--- space/models.py
+++ space/models.py
@@ -9,11 +9,11 @@ class Category(models.Model):
 
 class Spaces(models.Model):
 
-    title       = models.CharField(max_length=200, verbose_name="공간명")
-    address     = models.CharField(max_length=250, verbose_name="주소", null=True)
-    instagram   = models.CharField(max_length=200, verbose_name="인스타아이디", null=True)
-    category    = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
-    summary     = models.TextField(verbose_name="소개", null=True)
+    title       = models.CharField(max_length=200, verbose_name="공간명", unique=True)
+    address     = models.CharField(max_length=250, verbose_name="주소", null=True, blank=True)
+    instagram   = models.CharField(max_length=200, verbose_name="인스타아이디", null=True, blank=True)
+    category    = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
+    summary     = models.TextField(verbose_name="소개", null=True, blank=True)
     upload_sns  = models.BooleanField(verbose_name="인스타업로드", default=False)
     upload_app  = models.BooleanField(verbose_name="앱업로드", default=False)
     created_at  = models.DateTimeField(auto_now_add=True, verbose_name="작성일")
